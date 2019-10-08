44abcf1fcb8492adca3dd65374638a098852210d
League Student <league@iMac-9.attlocal.net>
2019-10-07 19:26:29 -0700

    I finished Binary converter

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -1,19 +1,34 @@
 package _07_binary_converter;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class BinaryConverter {
+public class BinaryConverter implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	JTextField answer;
+	JButton buttonConvert;
 	void createUI(){
 		frame = new JFrame("Binary Converter!");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(3);
+		frame.setPreferredSize(new Dimension (500,250));
 		panel = new JPanel();
 		answer = new JTextField();
+		answer.setPreferredSize(new Dimension(200,20));
+		buttonConvert = new JButton("Convert binary to ASCII!");
+		buttonConvert.addActionListener(this);
 		frame.add(panel);
+		panel.add(answer);
+		panel.add(buttonConvert);
+		frame.pack();
 		
 	}
 	String convert(String input) {
@@ -38,5 +53,11 @@ public class BinaryConverter {
 	public static void main(String[] args) {
 		BinaryConverter converter = new BinaryConverter();
 		converter.createUI();
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,convert(answer.getText()));
 	}
 }
